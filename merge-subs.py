c08fadff53973d02c7c40e64977d9009b7787dc5
KarukSophia <karuk@mailbox.org>
2021-01-22 15:20:26 +0100

    test

--- merge-subs.py
+++ merge-subs.py
@@ -40,14 +40,10 @@ class Sub:
 		self.timestamp = timestamp
 		self.id = id
 		self.langs = [[]]
-		time = ConvertSubs.timestamp_to_seconds(timestamp)
+		time = Sub.timestamp_to_seconds(timestamp)
 		self.st = time[0]
 		self.et = time[1]
 
-	def appendString(self, string):
-		self.strings.append(string)
-
-class ConvertSubs:
 	@staticmethod
 	def time_to_string(x, n):
 		if(x >= 10 ** (n-1)):
@@ -69,24 +65,24 @@ class ConvertSubs:
 		cs = round(cs, 3)
 		sec = math.floor(sec)
 
-		hour = ConvertSubs.time_to_string(round(hour), 2)
-		min = ConvertSubs.time_to_string(round(min), 2)
-		sec = ConvertSubs.time_to_string(round(sec), 2)
-		cs = ConvertSubs.time_to_string(round(1000*cs), 3)
+		hour = Sub.time_to_string(round(hour), 2)
+		min = Sub.time_to_string(round(min), 2)
+		sec = Sub.time_to_string(round(sec), 2)
+		cs = Sub.time_to_string(round(1000*cs), 3)
 
 		return [hour, min, sec, cs]
 
 	@staticmethod
 	def seconds_to_timestamp(st, et):
-		stl = ConvertSubs.seconds_to_time(st)
-		etl = ConvertSubs.seconds_to_time(et)
+		stl = Sub.seconds_to_time(st)
+		etl = Sub.seconds_to_time(et)
 		st_timestamp = stl[0]+':'+stl[1]+':'+stl[2]+'.'+stl[3]
 		et_timestamp = etl[0]+':'+etl[1]+':'+etl[2]+'.'+etl[3]
 		return st_timestamp+' --> '+et_timestamp
 
 	@staticmethod
 	def timestamp_to_seconds(timestamp):
-		times = ConvertSubs.timestamp_to_time(timestamp)
+		times = Sub.timestamp_to_time(timestamp)
 		start_time = int(times[0][0])*60*60 + int(times[0][1])*60 + int(times[0][2]) + int(times[0][3]) / 1000
 		end_time   = int(times[1][0])*60*60 + int(times[1][1])*60 + int(times[1][2]) + int(times[1][3]) / 1000
 		return [start_time, end_time]
@@ -103,52 +99,7 @@ class ConvertSubs:
 
 		return [stl, etl]
 
-#	@staticmethod
-#	def merge_subs(sub_dics, threshhold):
-#		return_dic = copy.deepcopy(sub_dics[0])
-#		return_dic['language'] = ''
-#		for sub_dic in sub_dics:
-#			is_first_sub_dic = (sub_dics.index(sub_dic) == 0)
-#			return_dic['subs']
-#			concat_strings = [[],[]]
-#			temp_times = [[-200, -200],[-200,-200]]
-#			if(not is_first_sub_dic):
-#				for sub_1 in sub_dics[0]['subs']:
-#					if(concat_strings[1] != []):
-#						if(concat_strings[0] == []):
-#							temp_times[0] = [sub_1.st, sub_1.et]
-#						for string in sub_1.langs[0]:
-#							concat_strings[0].append(string)
-#					concat_strings[1] = []
-#					for sub_2 in sub_dic['subs']:
-#						i = 0
-#						is_start_time_within_threshhold = sub_2.st-threshhold <= sub_1.st <= sub_2.st+threshhold
-#						is_end_time_within_threshhold = sub_2.et-threshhold <= sub_1.et <= sub_2.et+threshhold
-#						is_start_time_over_threshhold = sub_1.st <= sub_2.st+threshhold
-#						if(is_start_time_over_threshhold):
-#							if(temp_times[1] == [-200,-200]):
-#								temp_times[1] = [sub_2.st, sub_2.et]
-#							for string in sub_2.langs[0]:
-#								concat_strings[1].append(string)
-#						print(temp_times)
-#						if(is_end_time_within_threshhold):
-#							print('sub_2.st: '+str(sub_2.st))
-#							print('temp_times[1][0]: '+str(temp_times[1][0]))
-#							if(sub_2.st-threshhold <= temp_times[1][0] <= sub_2.st+threshhold):
-#								print('PASS')
-#								temp_times[1] = [-200, -200]
-#								index = sub_dics[0]['subs'].index(sub_1)
-#								return_dic['subs'][index].langs.append([])
-#								for string in concat_strings[1]:
-#									return_dic['subs'][index].langs[-1].append(string)
-#								break
-#						if(sub_dic['subs'].index(sub_2) == len(sub_dic['subs'])-1):
-#							print('ERROR')
-#							temp_times[1] = [-200, -200]
-#						else:
-#							i += 1
-#		return return_dic
-
+class MergeSubs:
 	@staticmethod
 	def merge_subs(sub_dics, threshhold):
 		return_dic={
@@ -174,27 +125,13 @@ class ConvertSubs:
 						if(test == True):
 							print('!!!ERROR!!!')
 						if(is_start_time_over_threshhold):
-							print(str(i)+': C')
 							for string in sub_2.langs[0]:
 								concat_strings[1].append(string)
 						if(times != []):
-							print(str(i)+': A')
 							is_start_time_within_threshhold = sub_2.st-threshhold <= times[0] <= sub_2.st+threshhold
 							is_end_time_within_threshhold = sub_2.et-threshhold <= sub_1.et <= sub_2.et+threshhold
-							#print('sub1_langs[0]:')
-							#print(sub_1.langs[0])
-							#print('concatstrings[0]:')
-							#print(concat_strings[0])
-							#print('concatstrings[1]:')
-							#print(concat_strings[1])
-							#print(str(sub_2.st-threshhold)+' <= '+str(times[0])+' <= '+str(sub_2.st+threshhold))
-							#print(str(sub_2.et-threshhold)+' <= '+str(sub_1.et)+' <= '+str(sub_2.et+threshhold))
 							if(is_start_time_within_threshhold and is_start_time_within_threshhold):
-								print(str(i)+': B')
-								return_dic['subs'].append(Sub(i+1, ConvertSubs.seconds_to_timestamp(times[0], sub_1.et)))
-								print(ConvertSubs.seconds_to_timestamp(times[0], sub_1.et))
-								print(times[0])
-								print(sub_1.et)
+								return_dic['subs'].append(Sub(i+1, Sub.seconds_to_timestamp(times[0], sub_1.et)))
 								return_dic['subs'][i].langs.append([])
 								for string in sub_1.langs[0]:
 									concat_strings[0].append(string)
@@ -209,7 +146,6 @@ class ConvertSubs:
 								i += 1
 								break
 						if(is_end_time_within_threshhold):
-							print(str(i)+': D')
 							index = sub_dics[0]['subs'].index(sub_1)
 							return_dic['subs'].append(Sub(i+1, sub_1.timestamp))
 							return_dic['subs'][i].langs.append([])
@@ -220,8 +156,6 @@ class ConvertSubs:
 							i += 1
 							break
 						if(sub_dic['subs'].index(sub_2) == len(sub_dic['subs'])-1):
-							print(str(i)+': E')
-							print()
 							test = True
 							for string in sub_1.langs[0]:
 								concat_strings[0].append(string)
@@ -229,37 +163,9 @@ class ConvertSubs:
 
 		return return_dic
 
-#	@staticmethod
-#	def merge_subs(sub_dics, threshhold):
-#		return_dic = copy.deepcopy(sub_dics[0])
-#		for sub_1 in return_dic['subs']:
-#			for sub_dic in sub_dics:
-#				is_first_sub_dic = (sub_dics.index(sub_dic) == 0)
-#				if(not is_first_sub_dic):
-#					i = 0
-#					append_strings = []
-#					print(len(sub_1.langs))
-#					sub_1.langs.append([])
-#					for sub_2 in sub_dic['subs']:
-#						times_1 = ConvertSubs.timestamp_to_seconds(sub_1.timestamp)
-#						times_2 = ConvertSubs.timestamp_to_seconds(sub_2.timestamp)
-#						is_start_time_over_threshhold = times_1[0] <= times_2[0]+threshhold
-#						is_end_time_within_threshhold = times_2[1]-threshhold <= times_1[1] <= times_2[1]+threshhold
-#						if(is_start_time_over_threshhold):
-#							for string in sub_2.langs[0]:
-#								append_strings.append(string)
-#						if(is_end_time_within_threshhold):
-#							for string in append_strings:
-#								sub_1.langs[-1].append(string)
-#							break
-#						if(i >= len(sub_dics[1]['subs'])):
-#							print('ERROR')
-#						else:
-#							i += 1
-#		return return_dic
-#
+class ManipulateSubs:
 	@staticmethod
-	def vtt_to_sub_dic(path):
+	def parse_vtt(path):
 		f = open(path, 'r')
 		lines = f.readlines()
 		subIndex=0
@@ -301,7 +207,7 @@ class ConvertSubs:
 		return(sub_dic)
 
 	@staticmethod
-	def create_concat_string_lrc(strings):
+	def create_concat_string(strings):
 		concat_string = ''
 		for string in strings:
 			ends_on_special_char = re.search('[!?\.,:;"\(\)~]$', string)
@@ -321,10 +227,10 @@ class ConvertSubs:
 		f = open(sub_dic['filename']+'.lrc', 'w')
 		seperator = '|'
 		for sub in sub_dic['subs']:
-			st = ConvertSubs.timestamp_to_time(sub.timestamp)[0]
+			st = Sub.timestamp_to_time(sub.timestamp)[0]
 			concat_string = ''
 			for lang in sub.langs:
-				concat_string += ConvertSubs.create_concat_string_lrc(lang)+' '+seperator+' '
+				concat_string += ManipulateSubs.create_concat_string(lang)+' '+seperator+' '
 			concat_string = concat_string.rstrip(' '+seperator)
 
 			f.write('['+str(st[1])+':'+str(st[2])+'.'+str(st[3])[:-1]+']'+concat_string+'\n')
@@ -337,7 +243,7 @@ class ConvertSubs:
 			wf.write(str(sub.id) + "\n")
 			wf.write(sub.timestamp + "\n")
 			for lang in sub.langs:
-				concat_string = ConvertSubs.create_concat_string_lrc(lang)
+				concat_string = ManipulateSubs.create_concat_string(lang)
 				wf.write(concat_string+'\n')
 			wf.write('\n')
 
@@ -353,6 +259,7 @@ class ConvertSubs:
 
 		return return_sub_dic
 
+class DownloadSubs:
 	@staticmethod
 	def ydl(url, langs):
 		ydl_opts = {
@@ -376,13 +283,14 @@ class ConvertSubs:
 url = args.yt_url
 langs = args.languages
 
-paths = ConvertSubs.ydl(url, langs)
+paths = DownloadSubs.ydl(url, langs)
+print(langs)
+print(paths)
 dics = []
 for path in paths:
-	dics.append(ConvertSubs.vtt_to_sub_dic(path))
-dic3 = ConvertSubs.merge_subs(dics, 0.75)
+	dics.append(ManipulateSubs.parse_vtt(path))
+dic3 = MergeSubs.merge_subs(dics, 0.75)
 if(args.remove_japanese_lines):
-	dic3 = ConvertSubs.remove_japanese_lines(dic3)
-ConvertSubs.write_lrc(dics[1])
-ConvertSubs.write_srt(dic3)
-
+	dic3 = ManipulateSubs.remove_japanese_lines(dic3)
+ManipulateSubs.write_lrc(dic3)
+ManipulateSubs.write_srt(dic3)
